3d26c4d28809ca733a2c70f802b9550e0e8dfff5
Tech Master <106694080+tech-master26@users.noreply.github.com>
2025-02-02 00:13:58 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo -e "\033[38;2;0;255;0m[+] Updating packages...\033[0m"
 pkg update -y && pkg upgrade -y
 
 echo -e "\033[38;2;0;255;0m[+] Installing required packages...\033[0m"
-pkg install -y python wget curl ncurses-utils git openssl nano 
+pkg install -y python wget curl ncurses-utils git openssl nano ffmpeg
 
 # Setup storage permissions
 echo -e "\033[38;2;0;255;0m[+] Setting up storage...\033[0m"
@@ -13,8 +13,10 @@ termux-setup-storage
 
 # Install Python dependencies
 echo -e "\033[38;2;0;255;0m[+] Installing Python libraries...\033[0m"
-pip install --upgrade pip
-pip install yt-dlp requests bs4 pytube 
+pip install yt-dlp
+pip install requests
+pip install bs4
+pip install pytube 
 
 # Create necessary directories
 echo -e "\033[38;2;0;255;0m[+] Creating directories...\033[0m"
